53ac719d50d3f3971547ca9062ba898463f644fc
amcdonald <e_dorion@hotmail.com>
2025-06-02 08:47:31 +1000

    run again without AWX

--- terraform/infra_setup/awx.tf
+++ terraform/infra_setup/awx.tf
@@ -1,25 +1,25 @@
 ####### inventory #######
-resource "awx_inventory" "default" {
-  name            = "terraform-demo"
-  organization    = "2"
-}
+#resource "awx_inventory" "default" {
+#  name            = "terraform-demo"
+#  organization    = "2"
+#}
 
 ######## Add Hosts to inventory ########
-resource "awx_inventory_host" "boundary" {
-  count        = length(aws_instance.boundary_instance)
-  name         = "boundary-${count.index}"
-  description  = "boundary-${count.index}"
-  instance_id  = resource.aws_instance.boundary_instance[count.index].id
-  inventory_id = resource.awx_inventory.default.id
-  enabled   = true
-  variables = <<YAML
----
-ansible_host: ${resource.aws_instance.boundary_instance[count.index].public_dns}
-YAML
-}
+#resource "awx_inventory_host" "boundary" {
+#  count        = length(aws_instance.boundary_instance)
+#  name         = "boundary-${count.index}"
+#  description  = "boundary-${count.index}"
+#  instance_id  = resource.aws_instance.boundary_instance[count.index].id
+#  inventory_id = resource.awx_inventory.default.id
+#  enabled   = true
+#  variables = <<YAML
+#---
+#ansible_host: ${resource.aws_instance.boundary_instance[count.index].public_dns}
+#YAML
+#}
 
 ######## kick of provision job ######
-resource "awx_job_template_launch" "boundary" {
-    job_template_id = "9"
-    inventory_id = resource.awx_inventory.default.id
-}
+#resource "awx_job_template_launch" "boundary" {
+#    job_template_id = "9"
+#    inventory_id = resource.awx_inventory.default.id
+#}
